da95b3494bbb1e14115869e5e9dfb150c901ae5e
Rafael Tivane <luislangabusiness@gmail.com>
2022-01-14 07:09:17 +0200

    creating services grid

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,36 +1,74 @@
-import { Grid } from '@mui/material'
+import { Button, Grid, Typography } from '@mui/material'
 import Card from './Card';
 import passionaleIllustration from '../../assets/images/home/illustration-passionate.svg'
 import resourcefulIllustration from '../../assets/images/home/illustration-resourceful.svg'
 import friendlyIllustration from '../../assets/images/home/illustration-friendly.svg'
-import classNames from 'classnames'; /*useResponsive, useTypography*/
+import classNames from 'classnames'; /*useResponsive*/
 import { useStyles } from './styles'
-import { useDisplay, } from '../../styles';
+import { useBackground, useDisplay, useTypography } from '../../styles';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { Link } from 'react-router-dom'
 
 const Home = () => {
+    const bg = useBackground();
     const display = useDisplay();
     const classes = useStyles();
+    const text = useTypography();
 
     return (
-        <Grid container className={classNames(display.px)}>
-            <Card 
-                description="Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions."
-                image={passionaleIllustration}
-                title="Passionate"
-            />
-            <Card 
-                avatarClassName={classNames(classes.resourcefulAvatar)}
-                description="Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs."
-                image={resourcefulIllustration}
-                title="Resouceful"
-            />
-            <Card 
-                avatarClassName={classNames(classes.friendlyAvatar)}
-                description=" We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide."
-                image={friendlyIllustration}
-                title="Friendly"
-            />
-        </Grid>
+        <>
+            <div className={classNames(classes.projectsGrid, display.px, display.mb3)}>
+                <div className={classNames(display.alignCenter, display.flex, display.flexColumn, display.justifyCenter, classes.project, bg.noReapeat, bg.center, bg.cover, classes.webDesign)}>
+                    <Typography component="h2" variant="h6" className={classNames(text.font7, text.textLight)}>
+                        Web Design
+                    </Typography>
+                    <Link to="/" className={classNames(display.mt1, display, text.noUnderline)}>
+                        <Button className={classNames(text.textLight)} endIcon={<ArrowForwardIosIcon className={classNames(classes.rightArrow)} />}>
+                            View projects
+                        </Button>
+                    </Link>
+                </div>
+                <div className={classNames(display.alignCenter, display.flex, display.flexColumn, display.justifyCenter, classes.project, bg.noReapeat, bg.center, bg.cover, classes.appDesign)}>
+                    <Typography component="h2" variant="h6" className={classNames(text.font7, text.textLight)}>
+                        App Design
+                    </Typography>
+                    <Link to="/" className={classNames(display.mt1, display, text.noUnderline)}>
+                        <Button className={classNames(text.textLight)} endIcon={<ArrowForwardIosIcon className={classNames(classes.rightArrow)} />}>
+                            View projects
+                        </Button>
+                    </Link>
+                </div>
+                <div className={classNames(display.alignCenter, display.flex, display.flexColumn, display.justifyCenter, classes.project, bg.noReapeat, bg.center, bg.cover, classes.graphicDesign)}>
+                    <Typography component="h2" variant="h6" className={classNames(text.font7, text.textLight)}>
+                        Graphic Design
+                    </Typography>
+                    <Link to="/" className={classNames(display.mt1, display, text.noUnderline)}>
+                        <Button className={classNames(text.textLight)} endIcon={<ArrowForwardIosIcon className={classNames(classes.rightArrow)} />}>
+                            View projects
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+            <Grid container className={classNames(display.px)}>
+                <Card 
+                    description="Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions."
+                    image={passionaleIllustration}
+                    title="Passionate"
+                />
+                <Card 
+                    avatarClassName={classNames(classes.resourcefulAvatar)}
+                    description="Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs."
+                    image={resourcefulIllustration}
+                    title="Resouceful"
+                />
+                <Card 
+                    avatarClassName={classNames(classes.friendlyAvatar)}
+                    description=" We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide."
+                    image={friendlyIllustration}
+                    title="Friendly"
+                />
+            </Grid>
+        </>
     )
 };
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,31 @@
 import { makeStyles } from "@mui/styles";
+import webDesignMobile from '../../assets/images/home/mobile/image-web-design.jpg'
+import appDesignMobile from '../../assets/images/home/mobile/image-app-design.jpg'
+import graphicDesignMobile from '../../assets/images/home/mobile/image-graphic-design.jpg'
 
 export const useStyles = makeStyles(theme => ({
+    projectsGrid: {
+        display: 'grid',
+        gridRowGap: 30,
+        gridTemplateColumns: '100%',
+        gridTemplateRows: '250px 250px 250px',
+    },
+    project: {
+        borderRadius: 30
+    },
+    webDesign: {
+        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, .1), rgba(0, 0, 0, .4)), url(${webDesignMobile})`
+    },
+    appDesign: {
+        backgroundImage: `url(${appDesignMobile})`
+    },
+    graphicDesign: {
+        backgroundImage: `url(${graphicDesignMobile})`
+    },
+    rightArrow: {
+        color: '#E7816B !important',
+        fontSize: '1rem !important'
+    },
     resourcefulAvatar: {
         "&::before": {
             transform: 'rotate(290deg)'
