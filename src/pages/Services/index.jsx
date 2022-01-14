8971e24944d01b0c6d16f29e68549ae67642ab03
Rafael Tivane <luislangabusiness@gmail.com>
2022-01-14 13:09:20 +0200

    making sevice tablet version responsive

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -1,8 +1,30 @@
+import { Typography } from "@mui/material";
+import classNames from "classnames";
+import { useBackground, useDisplay, useResponsive, useTypography } from '../../styles';
+import { useStyles } from './styles'
 
 
 const Services = () => {
+    const bg = useBackground();
+    const display = useDisplay();
+    const classes = useStyles();
+    const responsive = useResponsive();
+    const text = useTypography();
+
     return (
         <>
+            <section className={classNames(display.px,)}>
+                <div className={classNames(display.w100, display.flex, display.flexColumn, display.alignCenter, 
+                    bg.noRepeat, classes.hero, display.justifyCenter, bg.cover)}>
+                    <Typography component="h1" variant="h5" className={classNames(text.textLight, text.alignCenter )}>
+                        Web Design
+                    </Typography>
+                    <Typography className={classNames(text.textLight, text.alignCenter, text.rem9, display.mt1,
+                        classes.heroDescription)}>
+                        We build websites that serve as powerful marketing tools and bring memorable brand experiences.
+                    </Typography>
+                </div>
+            </section>
         </>
     );
 };
--- src/pages/Services/styles.js
+++ src/pages/Services/styles.js
@@ -1,5 +1,24 @@
 import { makeStyles } from "@mui/styles";
+import heroPattern from '../../assets/images/pattern/bg-pattern-two-circles.svg';
+import heroPatternTablet from '../../assets/images/pattern/bg-pattern-call-to-action.svg';
 
+const linearGradient = 'linear-gradient(to bottom, rgba(0, 0, 0, .1), rgba(0, 0, 0, .4))';
 export const useStyles = makeStyles(theme => ({
-    
+    hero: {
+        backgroundColor: '#E7816B',
+        backgroundImage: `url(${heroPattern})`,
+        backgroundPosition: 'right -43px bottom 23px',
+        borderRadius: 15,
+        height: 300,
+        [theme.breakpoints.up('sm')]: {
+            backgroundImage: `url(${heroPatternTablet})`,
+            backgroundPosition: 'right -43px center',
+            height: 250,
+        }
+    },
+    heroDescription: {
+        [theme.breakpoints.up('sm')]: {
+            maxWidth: '70%'
+        }
+    }
 }));
