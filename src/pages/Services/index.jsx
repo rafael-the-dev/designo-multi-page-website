1bb110bdf8dbc241d089e1166120814570b62903
Rafael Tivane <luislangabusiness@gmail.com>
2022-01-15 07:52:48 +0200

    making service page dynamic

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -3,9 +3,18 @@ import classNames from "classnames";
 import { useBackground, useDisplay, useResponsive, useTypography } from '../../styles';
 import { useStyles } from './styles'
 import Card from './Card'
-import expressImage from '../../assets/images/services/image-express.jpg'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import data from '../../services-data.json';
+import { useEffect, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import expressImage from '../../assets/images/services/web-design/image-express.jpg'
+import transferImage from '../../assets/images/services/web-design/image-transfer.jpg'
+import photonImage from '../../assets/images/services/web-design/image-photon.jpg'
+import blogrImage from '../../assets/images/services/web-design/image-blogr.jpg'
+import builderImage from '../../assets/images/services/web-design/image-builder.jpg'
+import campImage from '../../assets/images/services/web-design/image-camp.jpg'
 
 const Services = () => {
     const bg = useBackground();
@@ -14,6 +23,31 @@ const Services = () => {
     const responsive = useResponsive();
     const text = useTypography();
 
+    const { id } = useParams()
+    const [ service, setService ] = useState({});
+
+    useEffect(() => {
+        const result = data[id];
+        if(result) {
+            setService(result);
+        }
+    }, [ id ]);
+
+    const othersServicesImages = useRef({
+        'app-design': classes.appDesign,
+        'graphic-design': classes.graphicDesign,
+        'web-design': classes.webDesign,
+    });
+
+    const cardsImages = useRef({
+        "blogr-card": blogrImage,
+        "builder-card": builderImage,
+        "camp-card": campImage,
+        "express-card": expressImage,
+        "photon-card": photonImage,
+        "transfer-card": transferImage
+    })
+
     return (
         <>
             <section className={classNames(display.px,)}>
@@ -21,57 +55,55 @@ const Services = () => {
                     bg.noRepeat, classes.hero, display.justifyCenter, bg.cover)}>
                     <Typography component="h1" variant="h5" className={classNames(text.textLight, text.alignCenter,
                         text.font7, classes.heroTitle )}>
-                        Web Design
+                        { service.title }
                     </Typography>
                     <Typography className={classNames(text.textLight, text.alignCenter, text.rem9, display.mt1,
                         classes.heroDescription)}>
-                        We build websites that serve as powerful marketing tools and bring memorable brand experiences.
+                            { service.description }
                     </Typography>
                 </div>
             </section>
             <div className={classNames(display.px, classes.cardsContainer, responsive.mdMt2)}>
-                <Card 
-                    description="A multi-carrier shipping website for ecommerce businesses"
-                    image={expressImage}
-                    title="Express"
-                />
-                <Card 
-                    description="A multi-carrier shipping website for ecommerce businesses"
-                    image={expressImage}
-                    title="Express"
-                />
-                <Card 
-                    description="A multi-carrier shipping website for ecommerce businesses"
-                    image={expressImage}
-                    title="Express"
-                />
-                <Card 
-                    description="A multi-carrier shipping website for ecommerce businesses"
-                    image={expressImage}
-                    title="Express"
-                />
-                <Card 
-                    description="A multi-carrier shipping website for ecommerce businesses"
-                    image={expressImage}
-                    title="Express"
-                />
-                <Card 
-                    description="A multi-carrier shipping website for ecommerce businesses"
-                    image={expressImage}
-                    title="Express"
-                />
+                {
+                    service.cards ? (
+                        service.cards.map(item => (
+                            <Card 
+                                description={ item.description }
+                                image={ cardsImages.current[item.key]}
+                                title={ item.title }
+                            />
+                        ))
+                    ) : <></>
+                }
             </div>
             <div className={classNames(classes.projectsGrid, display.px, display.mt3, display.pt1, responsive.mdPt2, responsive.mdMb2)}>
-                <div className={classNames(display.alignCenter, display.flex, display.flexColumn, display.justifyCenter, classes.project, bg.noReapeat, bg.center, bg.cover, classes.webDesign)}>
-                    <Typography component="h2" variant="h6" className={classNames(text.font7, text.textLight)}>
-                        Web Design
-                    </Typography>
-                    <Link to="/" className={classNames(display.mt1, display, text.noUnderline)}>
-                        <Button className={classNames(text.textLight)} endIcon={<ArrowForwardIosIcon className={classNames(classes.rightArrow)} />}>
-                            View projects
-                        </Button>
-                    </Link>
-                </div>
+                {
+                    service['others services'] ? (
+                        service['others services'].map((item, index) => (
+                            <div key={index} className={classNames(display.alignCenter, display.flex, display.flexColumn, 
+                                display.justifyCenter, classes.project, bg.noReapeat, bg.center, bg.cover, 
+                                othersServicesImages.current[item.key])}>
+                                <Typography component="h2" variant="h6" className={classNames(text.font7, text.textLight)}>
+                                   { item.title }
+                                </Typography>
+                                <Link to={`/services/${item.link}`} className={classNames(display.mt1, display, text.noUnderline)}>
+                                    <Button className={classNames(text.textLight)} endIcon={<ArrowForwardIosIcon className={classNames(classes.rightArrow)} />}>
+                                        View projects
+                                    </Button>
+                                </Link>
+                            </div>
+                        ))
+                    ) : <></>
+                }
+            </div>
+        </>
+    );
+};
+
+export default Services;
+
+/**
+ * 
                 <div className={classNames(display.alignCenter, display.flex, display.flexColumn, display.justifyCenter, classes.project, bg.noReapeat, bg.center, bg.cover, classes.appDesign)}>
                     <Typography component="h2" variant="h6" className={classNames(text.font7, text.textLight)}>
                         App Design
@@ -82,9 +114,4 @@ const Services = () => {
                         </Button>
                     </Link>
                 </div>
-            </div>
-        </>
-    );
-};
-
-export default Services;
+ */
