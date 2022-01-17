edf52da47e71cf30a095d941e8086d24b700cc06
Rafael Tivane <luislangabusiness@gmail.com>
2022-01-17 07:50:01 +0200

    adding cards

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -2,9 +2,12 @@ import { Button, Grid, Typography } from '@mui/material'
 import classNames from 'classnames'; /**/
 import { useStyles } from './styles'
 import { useBackground, useDisplay, useResponsive, useTypography } from '../../styles';
+import Card from './Card'
+import australiaIcon from '../../assets/images/contact/illustration-australia.svg'
+import canadaIcon from '../../assets/images/contact/illustration-canada.svg'
+import ukIcon from '../../assets/images/contact/illustration-united-kingdom.svg'
 
 const Contact = () => {
-    const bg = useBackground();
     const display = useDisplay();
     const classes = useStyles();
     const responsive = useResponsive();
@@ -13,7 +16,21 @@ const Contact = () => {
     return (
         <>
             <Grid container className={classNames(display.px)}>
-                
+                <Card 
+                    image={canadaIcon}
+                    imageClassName={classes.canadaAvatar}
+                    title="Canada"
+                />
+                <Card 
+                    image={australiaIcon}
+                    imageClassName={classes.australiaAvatar}
+                    title="Australia"
+                />
+                <Card 
+                    image={ukIcon}
+                    imageClassName={classes.ukAvatar}
+                    title="United Kingdom"
+                />
             </Grid>
         </>
     );
--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -1,3 +1,19 @@
 import { makeStyles } from "@mui/styles";
 
-export const useStyles = makeStyles(theme => ({}));
+export const useStyles = makeStyles(theme => ({
+    australiaAvatar: {
+        "&::before": {
+            transform: 'rotate(25deg)'
+        }
+    },
+    canadaAvatar: {
+        "&::before": {
+            transform: 'rotate(71deg)'
+        }
+    },
+    ukAvatar: {
+        "&::before": {
+            transform: 'rotate(264deg)'
+        }
+    },
+}));
