9fc806d6e1b93f378f16ff7ba6f8b57a2742ba2c
Rafael Tivane <luislangabusiness@gmail.com>
2022-01-17 08:25:46 +0200

    creating hero section

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -8,6 +8,7 @@ import canadaIcon from '../../assets/images/contact/illustration-canada.svg'
 import ukIcon from '../../assets/images/contact/illustration-united-kingdom.svg'
 
 const Contact = () => {
+    const bg = useBackground();
     const display = useDisplay();
     const classes = useStyles();
     const responsive = useResponsive();
@@ -15,6 +16,25 @@ const Contact = () => {
 
     return (
         <>
+            <section className={classNames(display.px)}>
+                <div className={classNames(classes.hero, bg.noRepeat, display.pt3)}>
+                    <div>
+                        <Typography 
+                            component="h2" 
+                            variant="h5" 
+                            className={classNames(text.font7, text.alignCenter, text.textLight)}>
+                            Contact Us
+                        </Typography>
+                        <Typography className={classNames(text.rem9, display.mt1, text.alignCenter, text.textLight,
+                            display.opacity8)}>
+                            Ready to take it to the next level? Let’s talk about your project or idea 
+                            and find out how we can help your business grow. If you are looking for 
+                            unique digital experiences that’s relatable to your users, drop us a line.
+                        </Typography>
+                    </div>
+                    <form></form>
+                </div>
+            </section>
             <Grid container className={classNames(display.px)}>
                 <Card 
                     image={canadaIcon}
--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -1,6 +1,21 @@
 import { makeStyles } from "@mui/styles";
+import bgPattern from '../../assets/images/contact/bg-pattern-hero-contact-mobile.svg'
+import bgPatternDesktop from '../../assets/images/contact/bg-pattern-hero-desktop.svg'
 
 export const useStyles = makeStyles(theme => ({
+    hero: {
+        backgroundColor: '#E7816B',
+        backgroundImage: `url(${bgPattern})`,
+        backgroundSize: 775,
+        backgroundPosition: 'left -58px top',
+        borderRadius: 15,
+        height: 400,
+        paddingLeft: '3%',
+        paddingRight: '3%',
+        [theme.breakpoints.up('md')]: {
+            backgroundImage: `url(${bgPatternDesktop})`,
+        }
+    },
     australiaAvatar: {
         "&::before": {
             transform: 'rotate(25deg)'
