83dccee96ee91791b75f371fd03ddc39ca5ae804
Rafael Tivane <luislangabusiness@gmail.com>
2022-01-17 16:39:22 +0200

    adding responsiveness to tablet version

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -11,14 +11,15 @@ const About = () => {
     const text = useTypography();
 
     return (
-        <div >
+        <div className={classNames(classes.container)}>
             <section className={classNames(display.alignStretch, display.flex, display.flexColumn, display.w100, classes.hero)}>
                 <div className={classNames(bg.cover, bg.noRepeat, bg.center, classes.image, classes.heroImageContainer)}></div>
                 <div className={classNames(classes.heroContent, bg.noRepeat)}>
                     <Typography component="h1" variant="h5" className={classNames(text.textLight, text.alignCenter)}>
                         About us
                     </Typography>
-                    <Typography className={classNames(text.rem9, text.textLight, display.opacity9, text.alignCenter)}>
+                    <Typography className={classNames(text.rem9, text.textLight, display.opacity9, text.alignCenter,
+                        display.mt1, classes.description)}>
                         Founded in 2010, we are a creative agency that produces lasting results for 
                         our clients. We’ve partnered with many startups, corporations, and nonprofits 
                         alike to craft designs that make real impact. We’re always looking 
--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -2,21 +2,56 @@ import { makeStyles } from "@mui/styles";
 import heroImage from '../../assets/images/about/mobile/image-about-hero.jpg'
 import heroBgPattern from '../../assets/images/about/mobile/bg-pattern-hero-about-mobile.svg'
 
+
+import heroImageTablet from '../../assets/images/about/tablet/image-about-hero.jpg'
+import heroImageDesktop from '../../assets/images/about/desktop/image-about-hero.jpg'
+
 export const useStyles = makeStyles(theme => ({
+    container: {
+        [theme.breakpoints.up('sm')]: {
+            paddingLeft: '5%',
+            paddingRight: '5%'
+        },
+    },
     hero: {
         backgroundColor: '#E7816B',
+        [theme.breakpoints.up('sm')]: {
+            borderRadius: 15,
+        },
     },
     image: {
-        height: 250
+        height: 250,
+        [theme.breakpoints.up('sm')]: {
+            height: 320
+        }
     },
     heroImageContainer: {
         backgroundImage: `url(${heroImage})`,
-        objectFit: 'cover'
+        objectFit: 'cover',
+        [theme.breakpoints.up('sm')]: {
+            backgroundImage: `url(${heroImageTablet})`,
+            borderRadius: '15px 15px 0 0',
+        },
+        [theme.breakpoints.up('md')]: {
+            backgroundImage: `url(${heroImageDesktop})`,
+        }
     },
     heroContent: {
         backgroundImage: `url(${heroBgPattern})`,
         backgroundPosition: 'right 2px top -140px',
         paddingBottom: '4rem',
-        paddingTop: '4rem'
+        paddingTop: '4rem',
+        [theme.breakpoints.up('sm')]: {
+            backgroundPosition: 'left -818px top -230px',
+            backgroundSize: 1200,
+            borderRadius: '0 0 15px 15px'
+        },
+    },
+    description: {
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: 'auto !important',
+            marginRight: 'auto !important',
+            maxWidth: '70%'
+        }
     }
 }));
